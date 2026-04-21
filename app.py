d23b3dbe8a014f17c970958da8144cab9f9e9488
Paolam1903 <sernapaola922@gmail.com>
2026-04-21 08:02:37 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -33,7 +33,7 @@ if not RUTA_LIQ.exists() or not RUTA_METAS.exists():
 # =============================
 st.markdown("""
 <div style="background-color:#E30613;padding:15px;border-radius:10px">
-<h1 style="color:white;text-align:center">📊 Dashboard Cierre Comercial – CVS 2026</h1>
+<h1 style="color:white;text-align:center">📊 Dashboard Cierre Comercial final – CVS de marzo 2026</h1>
 </div>
 """, unsafe_allow_html=True)
 
